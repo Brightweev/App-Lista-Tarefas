640f788d6579588c9819bcf5e1fd74932816a86a
Matheus R <matheus.silva.r92@gmail.com>
2024-09-13 08:43:23 -0300

    fundamentos 2

---
+++ index.js
@@ -0,0 +1,16 @@
+// essas barras são para criar comentarios
+//console.log("Hello World");
+
+let meta = {
+    value: "ler um livro por mês",
+    checked: true,
+}
+
+let metas =[
+    meta,
+    {
+        value: "caminhar 20 minnutos",
+        checked: false,
+    }
+]
+console.log(metas(0).value)
